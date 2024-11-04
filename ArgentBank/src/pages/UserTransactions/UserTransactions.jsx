65f4a99167d55914acbbd89438efbe89b55fb14d
SopMI82 <sompydev@gmail.com>
2024-11-04 12:04:34 +0100

    intagration css et html terminée restera a finir la page details quand je serais en mesure de récupérer les données.

--- ArgentBank/src/pages/UserTransactions/UserTransactions.jsx
+++ ArgentBank/src/pages/UserTransactions/UserTransactions.jsx
@@ -1,7 +1,17 @@
+import Collapse from '../../components/Collapse/Collapse';
+import AccountOverview from './../../containers/AccountOverview/AccountOverview';
+
 const UserTransactions = () => {
     return (
         <div>
-            Liste des transactions
+            <main className="main bg-dark">
+            <AccountOverview
+                accountTitle="Argent Bank Checking (x8349)"
+                accountAmount="$2,082.79"
+                accountAmountDescr="Available Balance"
+            />
+            <Collapse></Collapse>
+            </main>
         </div>
     );
 };
