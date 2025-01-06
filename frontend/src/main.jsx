a1c0d3aa79f43ccf5fc0709e003708ce18eca0fd
SopMI82 <sompydev@gmail.com>
2025-01-06 17:37:33 +0100

    Correction Swagger

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,6 @@ import router from './Router'
 import { Provider } from 'react-redux';
 import store from './redux/store/store';
 
-// QUAND JE FERAI LA MISE EN PLACE DE REDUX :
-// ajouter "" et """ dans les imports
-// remplacer le contenu du routeur par "<Provider store = {store}><RouterProvider router={router}/></Provider>" a l'intérieur des balises StrictMode
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
